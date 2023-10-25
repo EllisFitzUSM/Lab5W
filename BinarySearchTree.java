25a34d23bfe3e4a017e426868d40f9d74a435d8e
EllisFitzUSM <144267279+EllisFitzUSM@users.noreply.github.com>
2023-10-25 14:23:04 -0400

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,5 +1,14 @@
 public abstract class BinarySearchTree {
+  class Node<E>() {
+    E data;
+    parent Node<E>;
+    left Node<E>;
+    right Node<E>;
+  }
+  
   public int data;
+  public int size;
+  public Node<E> root;
 
   public abstract boolean add(int data);
 
